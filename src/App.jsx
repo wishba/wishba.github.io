4bfe4ab3304d4fdd517866f56e2598c165a171f8
wishba <wisnubayu34@gmail.com>
2022-11-01 15:34:17 +0700

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Card from './Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const main = {
+    maxWidth: '22.5rem',
+    display: 'grid',
+    margin: 'auto'
+  }
+  const gridContainer = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    placeItems: 'center',
+    gap: '1rem'
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React count</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div style={main}>
+      <header>
+        <h1>
+          Hello! My Name Is
+          <br />
+          Wisnu Bayu
+        </h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          I am an informatics engineering student who really likes the field of front-end development.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+
+      <main>
+        <section>
+          <h2>Recent Project</h2>
+          <div style={gridContainer}>
+            <Card
+              image='/sc-sqr-advice.png'
+              title='Advice Generator App'
+              link='https://wishba.github.io/advice-generator-app-main/'
+            />
+            <Card
+              image='sc-sqr-news.png'
+              title='News Homepage'
+              link='https://wishba.github.io/news-homepage-main/'
+            />
+            <Card
+              image='sc-sqr-sunny.png'
+              title='Sunnyside agency landing page'
+              link='https://wishba.github.io/sunnyside-agency-landing-page-main/'
+            />
+            <Card
+              image='sc-sqr-time.png'
+              title='Time Tracking Dashboard App'
+              link='https://wishba.github.io/time-tracking-dashboard-main/'
+            />
+          </div>
+        </section>
+
+        <section>
+          <h2>Socials</h2>
+          <div style={gridContainer}>
+            <a href="https://twitter.com/wishba_" target="_blank" rel="noopener noreferrer">
+              twitter @wishba_
+            </a>
+            <a href="https://github.com/wishba" target="_blank" rel="noopener noreferrer">
+              github @wishba
+            </a>
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
