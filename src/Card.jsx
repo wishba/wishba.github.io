27d0460c099249b8e47e28b8622cb46d3d0aa73a
wishba <wisnubayu34@gmail.com>
2022-11-03 11:41:15 +0700

    hover link

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 function Card(props) {
   return (
     <div className='card'>
-      <img
-        className='card__image'
-        src={props.image}
-        alt={props.image}
-      />
-      <br />
-      <a href={props.link} target="_blank" rel="noopener noreferrer">{props.title}</a>
+      <a
+        className='card__link'
+        href={props.link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className='card__image'
+          src={props.image}
+          alt={props.image}
+        />
+        {props.title}
+      </a>
     </div>
   )
 }
